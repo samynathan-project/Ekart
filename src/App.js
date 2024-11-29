1042dc922035928974f651ac338ac54efdd9a24e
samynathan_project <samiradha2004@gmail.com>
2024-11-29 12:01:49 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header';
 import { Home } from './components/Home';
@@ -11,7 +11,7 @@ function App() {
   const [cart, setCart] = useState([]);
   return (
      <>
-        <HashRouter>
+        <BrowserRouter>
         <Header cart={cart} />
         <div className='container'>
           <Routes>
@@ -21,7 +21,7 @@ function App() {
             <Route path='/Register' element={<Register cart= {cart} setCart= {setCart} />} />
           </Routes>
         </div>
-        </HashRouter>
+        </BrowserRouter>
      </>
   );
 }
